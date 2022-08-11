52b18d6d476259ad2fe88b566b88c683f71c14e4
lucas.perno <lucas.perno@hotmail.com>
2022-08-11 20:44:10 -0300

    Logica Ecommerce con JS

--- js/main.js
+++ js/main.js
@@ -1,65 +1,145 @@
-let carritoDeCompras = []
+// DOM ACCESS DECLARATIONS
+let productsGrid = document.getElementById('grid-productos') // productsGrid
+let cartContainer = document.getElementById('contenedor-carrito') // productsContainer
+let countCartBadge = document.getElementById('contadorCarrito') // countCartBadge
+let totalCart = document.getElementById('precioTotal') // totalCart
 
-let contenedorProductos = document.getElementById('grid-productos')
-let contenedorCarrito = document.getElementById('contenedor-carrito')
-let contadorCarrito = document.getElementById('contadorCarrito')
-let total = document.getElementById('precioTotal')
 
-mostrarProductos()
+// Store declarations
+let cart = [] // cart
+const products = [] // products
 
-//Logica Ecommerce
-function mostrarProductos(){
-  
-  stockProductos.forEach(item =>{
-    let div = document.createElement('div')
-    div.className = 'producto'
-    div.innerHTML = `
-    <img class="imagen" src=${item.imagen} alt="">
-    <h3>${item.nombre}<h3>
-    <p>Categoria: ${item.categoria}</p>
-    <p class="precioProducto">Precio: $ ${item.precio}</p>
-    <button id="agregar${item.id}" class="boton-agregar">Agregar <i class="fas fa-shopping-cart"></i></button>
-    `
-    
-  contenedorProductos.appendChild(div)
-  const boton = document.getElementById (`agregar${item.id}`)
-   boton.addEventListener('click', () => { 
-    if (item.stock > 0) {
-      agregarAlCarrito(item.id)
+
+
+// Get data
+async function getData () {
+  fetch('https://api.mercadolibre.com/sites/MLA/search?q=auricularesgamer&limit=15')
+  .then(res => {return res.json()})
+  .then(data => {    
+    // Insert data from ML
+    data.results.forEach((result) => {
+      productsGrid.innerHTML += templateProduct(result)
+    })
+  }) 
+}
+
+getData()
+
+// Render HTML Product
+const templateProduct = (product) => {
+  return `
+    <div class="product">
+      <img class="imagen" src=${product.thumbnail} alt="">
+      <h3>${product.title}<h3>
+      <p>Categoria: ${product.category_id}</p>
+      <p class="precioProducto">Precio: $ ${product.price}</p>
+      <p class="precioProducto">Stock: ${product.available_quantity}</p>
+      <button class="boton-agregar" onclick='addToCart(${JSON.stringify(product)})'>Agregar <i class="fas fa-shopping-cart"></i></button>
+    </div>
+  `
+}
+
+
+// Add to cart
+function addToCart(product) {
+  // Check if has stock
+  if(product.available_quantity > 0) {
+    if(cart.find((prod) => prod.id === product.id)){
+      // Update product quantities
+      const indexProd = cart.findIndex((prod) => prod.id === product.id)
+      // If product added quantity is minor to the stock
+      if(cart[indexProd].added_quantity < cart[indexProd].available_quantity) {
+        cart[indexProd] = {...cart[indexProd], added_quantity: cart[indexProd].added_quantity + 1}
+      } else {
+        Swal.fire({
+          icon: 'error',
+          title: 'Oops...',
+          text: 'No tenemos mas stock de este producto!',
+        })
+      }
+    } else {
+      // Add product to cart
+      cart.push({...product, added_quantity: 1})
     }
-    else {
+    // Cart DOM Update
+    updateCart()
+  }
+  else {
+    // Show non-stock alert
     Swal.fire({
       icon: 'error',
       title: 'Oops...',
       text: 'Por el momento este articulo se encuentra sin stock!',
-    })}
-   })
-  })
-  
+    })
+  }
 }
 
-function agregarAlCarrito(id) {
-  let encontrado = stockProductos.find(element => element.id === id)
-  carritoDeCompras.push(encontrado)
-  mostrarCarrito(encontrado)
-  actualizarCarrito()
-  
-}
 
-function mostrarCarrito(encontrado) {
-  let div = document.createElement('div')
-  div.className = 'productoEnCarrito'
-  div.innerHTML = `
-  <p>${encontrado.nombre}</p>
-  <p>$ ${encontrado.precio}</p>
-  <button class="boton-eliminar"><i class="fas fa-trash-alt"></i></button>
+// Show cart
+const templateCart = (product) => {
+  return `
+    <div class="productoEnCarrito">
+      <p>${product.title}</p>
+      <p>$ ${product.price}</p>
+      <div class="container-quantity">
+        <button onclick='removeQuantity(${JSON.stringify(product.id)})'>-</button>
+        <span>${product.added_quantity}</span>
+        <button onclick='addQuantity(${JSON.stringify(product.id)})'>+</button>
+      </div>
+      <button class="boton-eliminar" onclick='deleteProductFromCart(${JSON.stringify(product.id)})'><i class="fas fa-trash-alt"></i></button>
+    </div>
   `
-  contenedorCarrito.appendChild(div)
 }
 
-function actualizarCarrito () {
-  contadorCarrito.innerText = carritoDeCompras.length
-  total.innerText = carritoDeCompras.reduce((acc, el)=> acc + el.precio, 0)
+// Render Cart Product
+function renderCartProducts (){
+  cartContainer.innerHTML = ""
+  cart.forEach((cartProd) => {
+    cartContainer.innerHTML += templateCart(cartProd)
+  })
+}
+
+// Update cart
+function updateCart () {
+  let totalItems = 0
+  for (let item in cart) {
+    totalItems += cart[item].added_quantity;
+  }
+  countCartBadge.innerText = totalItems
+  totalCart.innerText = cart.reduce((acc, product)=> acc + product.price * product.added_quantity, 0)
+  // Cart DOM Update
+  renderCartProducts()
+}
+
+// Delete Cart
+function deleteProductFromCart (prodId) {
+  const newCart = cart.filter((prod) => prod.id !== prodId )
+  cart = newCart
+  // Cart DOM Update
+  updateCart()
 }
 
 
+function addQuantity(id){
+  const indexProduct = cart.findIndex((prod) => prod.id === id)
+  // If product added quantity is minor to the stock
+  if(cart[indexProduct].added_quantity < cart[indexProduct].available_quantity){
+    cart[indexProduct].added_quantity = cart[indexProduct].added_quantity + 1
+  } else {
+    Swal.fire({
+      icon: 'error',
+      title: 'Oops...',
+      text: 'No tenemos mas stock de este producto!',
+    })
+  }
+  updateCart()
+}
+function removeQuantity(id){
+  const indexProduct = cart.findIndex((prod) => prod.id === id)
+  if(cart[indexProduct].added_quantity === 1) {
+    deleteProductFromCart(id)
+  } else {
+    cart[indexProduct].added_quantity = cart[indexProduct].added_quantity - 1
+  }
+  updateCart()
+}
--- js/stock.js
+++ js/stock.js
@@ -4,7 +4,9 @@ let stockProductos = [
   {id: 3, nombre: "REDRAGON Zeus H510", imagen:"/imagenes/Redragon-Zeus-H510-3.jpg", categoria: "Auriculares", precio: 12000, stock: 0},
   {id: 4, nombre: "LOGITECH G203", imagen:"/imagenes/MOU454.jpg", categoria: "Mouse", precio: 4000, stock: 0},
   {id: 5, nombre: "RAZER Blackwidow V3", imagen:"/imagenes/razer-blackwidow-V3.jpg", categoria: "Teclados", precio: 14000, stock: 5},
-  {id: 6, nombre: "MSI RX570 8GB", imagen:"/imagenes/msi-rx570.png", categoria: "Placa de Video", precio: 75000, stock: 3}
+  {id: 6, nombre: "HYPERX Pulsefire Surge", imagen:"/imagenes/hyperx-pulsefire-surge.jpg", categoria: "Mouse", precio: 7800, stock: 2},
+  {id: 7, nombre: "Zotac 3070ti Trinity", imagen:"/imagenes/zotac-3070ti-trinity.jpg", categoria: "Placa de Video", precio: 215000, stock: 10},
+  {id: 8, nombre: "MSI RX570 8GB", imagen:"/imagenes/msi-rx570.png", categoria: "Placa de Video", precio: 75000, stock: 3}
 ]
 
 const productosStorage = (item, producto1) => {localStorage.setItem("item", producto1) };
